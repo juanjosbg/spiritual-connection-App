7f2fb801517cba3399b34305ce593f8066fb812c
juanjosbg <juanjoseborrero95@gmail.com>
2025-10-15 16:55:37 -0500

    first commit

--- src/components/MeditationSection.tsx
+++ src/components/MeditationSection.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Card } from "../components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import { Play, Pause, RotateCcw, Clock } from "lucide-react";
 
 const meditations = [
